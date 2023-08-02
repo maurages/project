c3e9ed2602b35fd7e9239aafc96fc0226f973c79
Mau <mespiritu207@gmail.com>
2023-08-02 12:52:02 -0600

    Initial Commit

---
+++ project0/project0App.swift
@@ -0,0 +1,17 @@
+//
+//  project0App.swift
+//  project0
+//
+//  Created by maø on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct project0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
